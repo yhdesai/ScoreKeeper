a8a2222aff5c97754a939c7060d2bb95a01f82ed
Yhdesai <yhdesai99@ymail.com>
2017-05-23 20:01:56 +0530

    Java files edited

--- app/src/main/java/com/a16mb/techseries/scorekeeper/MainActivity.java
+++ app/src/main/java/com/a16mb/techseries/scorekeeper/MainActivity.java
@@ -1,12 +1,13 @@
 package com.a16mb.techseries.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    int scoreA = 0;
-    int scoreB = 0;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,232 +17,65 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void aplusone(View v) {
-        scoreA = scoreA + 1;
-        displayTeamA(scoreA);
+        scoreTeamA = scoreTeamA + 1;
+        displayTeamA(scoreTeamA);
+    }
+
+    public void aplustwo(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayTeamA(scoreTeamA);
     }
 
 
+    public void aplusthree(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayTeamA(scoreTeamA);
+    }
+
+    public void aplussix(View v) {
+        scoreTeamA = scoreTeamA + 6;
+        displayTeamA(scoreTeamA);
+    }
 
 
+    public void bplusone(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayTeamB(scoreTeamB);
+    }
 
 
+    public void bplustwo(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayTeamB(scoreTeamB);
+    }
 
 
+    public void bplusthree(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayTeamB(scoreTeamB);
+    }
 
+    public void bplussix(View v) {
+        scoreTeamB = scoreTeamB + 6;
+        displayTeamB(scoreTeamB);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void reset(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayTeamA(scoreTeamA);
+        displayTeamB(scoreTeamB);
+    }
 
 
     public void displayTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.scoreA);
         scoreView.setText(String.valueOf(score));
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public void resetScore(View v) {
-            scoreTeamA = 0;
-            scoreTeamB = 0;
-            displayForTeamA(scoreTeamA);
-            displayForTeamB(scoreTeamB);
-        }
-
-
+    public void displayTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.scoreB);
+        scoreView.setText(String.valueOf(score));
+    }
 }
